492484a83171e234276c041ae915b530942cc401
NateS <nateskiles94@gmail.com>
2021-08-12 21:41:19 -0700

    validator

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,10 +1,11 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 // Define and use a function in a new file
 
 const notes = getNotes()
-
 console.log(notes)
 
+console.log(validator.isURL('http://nateskiles.me'))
 
 
 
