2efb9fb365bc960b65f1179b7977da298846bd45
Nathan Skiles <nateskiles94@gmail.com>
2021-08-21 23:50:27 -0700

    yargs

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,13 +1,58 @@
 const chalk = require('chalk');
+const { describe } = require('yargs');
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
+// const command = process.argv[2]
 
-const command = process.argv[2]
+// Version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
 
-if (command === 'add') {
-    console.log('Adding Note')
-}
-else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Remove a note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Read a note')
+    }
+})
+console.log(yargs.argv)
+
+
+
+
+
+// console.log(process.argv)
+
+// if (command === 'add') {
+//     console.log('Adding Note')
+// }
+// else if (command === 'remove') {
+//     console.log('Removing note!')
+// }
