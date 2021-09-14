d6e35f71efb5155ccb90613507aea73ef1d9c9cd
NateS <nateskiles94@gmail.com>
2021-09-14 15:54:37 -0700

    setup

--- node-course/playground/1-json.js
+++ node-course/playground/1-json.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 // 3. Stringify the changed object and overwrite the original data
 // 4. Test your work by viewing data in the JSON file
 
-const dataBuffer = fs.readFileSync('2-join.json')
+const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
 
@@ -18,4 +18,4 @@ data.name = 'Nate'
 data.age = 27
 
 const userJson = JSON.stringify(data)
-fs.writeFileSync('2-join.json', userJson)
+fs.writeFileSync('1-json.json', userJson)
