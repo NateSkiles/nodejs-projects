e058c39d65c643f72af393b679baae23e4327d79
Nathan Skiles <nateskiles94@gmail.com>
2022-03-01 21:56:42 -0800

    weatherstack API connect

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,7 +1,22 @@
-console.log('Starting')
+const dotenv = require('dotenv').config();
+const request = require('postman-request');
+const moment = require('moment');
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-},2000)
+// Weatherstack API Key
+const API_KEY = process.env.API_KEY
+// API Call
+const query = "45.5152,-122.6784&units=f"
+const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${query}`
 
-console.log('Stopping')
+const options = {
+    url: url,
+    json: true
+}
+
+request(options, (error, response) => {
+    let current = response.body.current
+    let location = response.body.location
+
+    console.log(`Currently in ${location.name} ${location.region}:\n\tIt is ${current.temperature}℉ out and feels like ${current.feelslike}℉\n\tThe local time is: ${moment(location.localtime).format('LT')}`);
+})
+
