dba5df532451a21900a18c09959b36d83c6c4270
Nathan Skiles <nateskiles94@gmail.com>
2022-03-01 22:29:09 -0800

    mapbox init

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -3,20 +3,34 @@ const request = require('postman-request');
 const moment = require('moment');
 
 // Weatherstack API Key
-const API_KEY = process.env.API_KEY
+const WS_TOKEN = process.env.API_KEY_WS
+// Mapbox token
+const MB_TOKEN = process.env.API_KEY_MB
+
 // API Call
 const query = "45.5152,-122.6784&units=f"
-const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${query}`
+const weather_url = `http://api.weatherstack.com/current?access_key=${WS_TOKEN}&query=${query}`
+const location_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Portland.json?access_token=${MB_TOKEN}`
 
-const options = {
-    url: url,
+const location_options = {
+    url: location_url,
     json: true
 }
 
-request(options, (error, response) => {
-    let current = response.body.current
-    let location = response.body.location
+const weather_options = {
+    url: weather_url,
+    json: true
+}
 
-    console.log(`Currently in ${location.name} ${location.region}:\n\tIt is ${current.temperature}℉ out and feels like ${current.feelslike}℉\n\tThe local time is: ${moment(location.localtime).format('LT')}`);
+request(location_options, (err, res) => {
+    let data = res.body.features[0]
+
+    console.log(`Portland's location:\n\tLat: ${data.center[1]}\n\tLong: ${data.center[0]}`)
 })
 
+// request(weather_options, (error, response) => {
+//     let current = response.body.current
+//     let location = response.body.location
+
+//     console.log(`Currently in ${location.name} ${location.region}:\n\tWeather: ${current.weather_descriptions[0]}\n\tTemp: ${current.temperature}℉ and feels like ${current.feelslike}℉\n\tLocal time is: ${moment(location.localtime).format('LT')}`);
+// })
