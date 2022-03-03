0f3bf09b48f6c25a89b7cbbdaf9d226bc9c001a2
Nathan Skiles <nateskiles94@gmail.com>
2022-03-02 22:14:47 -0800

    add geocode util

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,40 +1,33 @@
 const dotenv = require('dotenv').config();
 const request = require('postman-request');
 const moment = require('moment');
+const geocode = require('./utils/geocode');
 
 // Weatherstack API Key
-const WS_TOKEN = process.env.API_KEY_WS
-// Mapbox token
-const MB_TOKEN = process.env.API_KEY_MB
+// const WS_TOKEN = process.env.API_KEY_WS
 
-// API Call
-const query_latlong = '45.5152,-122.6784&units=f'
-const query_location = 'Portland'
-const weather_url = `http://api.weatherstack.com/current?access_key=${WS_TOKEN}&query=${query_latlong}`
-const location_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query_location}.json?limit=1&access_token=${MB_TOKEN}`
 
-const location_options = {
-    url: location_url,
-    json: true
-}
+// // API Call
+// const query_latlong = '45.5152,-122.6784&units=f'
+// const weather_url = `http://api.weatherstack.com/current?access_key=${WS_TOKEN}&query=${query_latlong}`
 
-const weather_options = {
-    url: weather_url,
-    json: true
-}
+// const weather_options = {
+//     url: weather_url,
+//     json: true
+// }
 
-request(location_options, (err, res) => {
-    if (err) {
-        console.log('Unable to connect to location service. 🙁');
-    } else if (res.body.error) {
-        console.log('Unable to find location 🕵️‍♂️');
-    } else if (res.body.features.length === 0) {
-        console.log('No locations found, try searching for a new location.')
-    } else {
-        let data = res.body.features[0]
-        console.log(`${data.location}'s location:\n\tLat: ${data.center[1]}\n\tLong: ${data.center[0]}`)
-    }
-})
+// request(location_options, (err, res) => {
+//     if (err) {
+//         console.log('Unable to connect to location service. 🙁');
+//     } else if (res.body.error) {
+//         console.log('Unable to find location 🕵️‍♂️');
+//     } else if (res.body.features.length === 0) {
+//         console.log('No locations found, try searching for a new location.')
+//     } else {
+//         let data = res.body.features[0]
+//         console.log(`${data.location}'s location:\n\tLat: ${data.center[1]}\n\tLong: ${data.center[0]}`)
+//     }
+// })
 
 // request(weather_options, (err, res) => {
 //     if (err) {
@@ -48,3 +41,9 @@ request(location_options, (err, res) => {
 //         console.log(`Currently in ${location.name} ${location.region}:\n\tWeather: ${current.weather_descriptions[0]}\n\tTemp: ${current.temperature}℉ and feels like ${current.feelslike}℉\n\tLocal time is: ${moment(location.localtime).format('LT')}`);
 //     }
 // })
+
+
+geocode('Portland', (e, data) => {
+    console.log('Error', e);
+    console.log('Data', data)
+})
---
+++ node-course/weather-app/utils/geocode.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv').config();
+const request = require('postman-request');
+
+// Mapbox token
+const API_KEY = process.env.API_KEY_MB
+
+
+
+const geocode = (address, callback) => {
+    // Encode to address to URI
+    const query = encodeURIComponent(address)
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=1&access_token=${API_KEY}`
+    const options = {
+        url: url,
+        json: true
+    }
+
+    request(options, (err, res) => {
+        if (err) {
+            callback('Unable to connect to location service. 🙁', undefined)
+        } else if (res.body.features.length === 0) {
+            callback('No locations found, try searching for a new location.', undefined)
+        } else {
+            callback(undefined, {
+                longitude: res.body.features[0].center[0],
+                latitude: res.body.features[0].center[1]
+            })
+        }
+    })
+}
+
+module.exports = geocode
