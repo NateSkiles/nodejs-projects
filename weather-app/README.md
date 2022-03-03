70aac5a4592e03b2f1ee52b6722c6dfffe22a6d0
Nathan Skiles <nateskiles94@gmail.com>
2022-03-02 23:58:53 -0800

    update readme

--- weather-app/README.md
+++ weather-app/README.md
@@ -2,13 +2,24 @@
 
 Using the Weatherstack and Mapbox API's, pass a location to the app and recieve time/temp data about it.
 
+### Install app:
 ```$ npm install```
+
+### Run App:
+Pass a location to the app as an argument.
 ```
 $ node app.js Portland
 //
 // ->
-// Currently in Portland Oregon:
+// Currently in Portland, Oregon:
 //      Weather: Light Rain
 //      Temp: 50℉ and feels like 50℉
 //      Local time is: 11:50 PM
+```
+For cities with spaces, add quotations ```" "``` around the text or simple enter the full name as an argument.
+```terminal
+$ node app.js New York     
+Currently in Long Island, City New York:
+        Weather: Overcast
+        Temp: 46℉ and feels like 41℉
 ```
--- weather-app/app.js
+++ weather-app/app.js
@@ -19,7 +19,7 @@ if (!location) {
             //console.log(`Currently in ${location.name} ${location.region}:\n\tWeather: ${current.weather_descriptions[0]}\n\tTemp: ${current.temperature}℉ and feels like ${current.feelslike}℉\n\tLocal time is: ${moment(location.localtime).format('LT')}`);
     
             console.log(
-                `Currently in ${data.name} ${data.region}:\n\tWeather: ${data.description}\n\tTemp: ${data.temperature}℉ and feels like ${data.feelsLike}℉\n\tLocal time is: ${data.localTime}`
+                `Currently in ${data.name}, ${data.region}:\n\tWeather: ${data.description}\n\tTemp: ${data.temperature}℉ and feels like ${data.feelsLike}℉\n\tLocal time is: ${data.localTime}`
             );
         })
     })
