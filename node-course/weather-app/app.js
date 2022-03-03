ef1441000165299722ebae70918a9c431965fbef
Nathan Skiles <nateskiles94@gmail.com>
2022-03-02 23:12:29 -0800

    finish refactor

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,12 +1,20 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Portland', (e, data) => {
-    console.log('Error', e);
-    console.log('Data', data)
+geocode('Portland', (err, locationData) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    forecast(locationData.latitude, locationData.longitude, (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        //console.log(`Currently in ${location.name} ${location.region}:\n\tWeather: ${current.weather_descriptions[0]}\n\tTemp: ${current.temperature}℉ and feels like ${current.feelslike}℉\n\tLocal time is: ${moment(location.localtime).format('LT')}`);
+
+        console.log(
+            `Currently in ${data.name} ${data.region}:\n\tWeather: ${data.description}\n\tTemp: ${data.temperature}℉ and feels like ${data.feelsLike}℉\n\tLocal time is: ${data.localTime}`
+        );
+    })
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
