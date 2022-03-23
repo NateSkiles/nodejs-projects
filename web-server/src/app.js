0d0135d51ef03ac8cac2722a640dc9fb93f5baf0
Nathan Skiles <nateskiles94@gmail.com>
2022-03-22 21:29:54 -0700

    add handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,12 +4,34 @@ const express = require('express');
 console.log(path.join(__dirname, '../public'));
 
 const app = express();
-const publicDirPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirPath))
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates')
+
+// Sets handlebars as view engine
+app.set('view engine', 'hbs');
+// Templates path
+app.set('views', viewsPath)
+app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    res.render('index', {
+        title: 'weather',
+        name: 'Nate'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Nate'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Hey, listen!'
+    })
 })
 
 app.get('/weather', (req, res) => {
