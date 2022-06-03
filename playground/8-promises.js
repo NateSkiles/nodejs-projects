3fb147db8dbd945dadbe6fc0c238ba482a850525
Nathan Skiles <nateskiles94@gmail.com>
2022-06-02 19:35:27 -0700

    promise chaing example

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,17 +1,17 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    const user = { name: 'Nate' }
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     const user = { name: 'Nate' }
 
-    setTimeout(() => {
-        reject(console.log(`${user.name}, something went wrong.`))
-        resolve(console.log(`${user.name}`))
-    }, 2000);
-})
+//     setTimeout(() => {
+//         reject(console.log(`${user.name}, something went wrong.`))
+//         resolve(console.log(`${user.name}`))
+//     }, 2000);
+// })
 
-doWorkPromise
-    .then((result) => { console.log('Success ✅', result)})
-    .catch((err) => { console.log('Error!');})
+// doWorkPromise
+//     .then((result) => { console.log('Success ✅', result)})
+//     .catch((err) => { console.log('Error!');})
 
-console.log('Hi');
+// console.log('Hi');
 
 
 // 
@@ -22,3 +22,13 @@ console.log('Hi');
 //                                          \
 //                                            rejected
 //  
+
+// Promise chaining
+add(1, 1).then((sum) => {
+    console.log(sum);
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2);
+}).catch((e) => {
+    console.log(e);
+})
